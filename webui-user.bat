ae6454e0e90169040998a2d5a7b19bdad7ee8ac7
johnsonsiumen <31721748+yu-chun-kit@users.noreply.github.com>
2024-11-23 17:45:37 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,18 +1,22 @@
 @echo off
 
-set PYTHON=
-set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+@REM set PYTHON=
+@REM set GIT=
+@REM set VENV_DIR=
+@REM set COMMANDLINE_ARGS=
 
 @REM Uncomment following code to reference an existing A1111 checkout.
-@REM set A1111_HOME=Your A1111 checkout dir
-@REM
-@REM set VENV_DIR=%A1111_HOME%/venv
-@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
-@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
-@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
-@REM  --embeddings-dir %A1111_HOME%/embeddings ^
-@REM  --lora-dir %A1111_HOME%/models/Lora
+set A1111_HOME=C:/novelAI/novelai-webui-10-9update
+
+set PYTHON=%A1111_HOME%/venv/Scripts/python.exe
+set "GIT=C:/Program Files/Git/cmd/git.exe"
+set VENV_DIR=%A1111_HOME%/venv
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+ --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+ --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+ --embeddings-dir %A1111_HOME%/embeddings ^
+ --lora-dir %A1111_HOME%/models/Lora ^
+ --vae-path %A1111_HOME%/models/VAE ^
+ --text-encoder-dir %A1111_HOME%/models/text_encoder
 
 call webui.bat
